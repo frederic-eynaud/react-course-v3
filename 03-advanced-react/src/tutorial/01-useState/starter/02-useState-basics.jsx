71dbc97631a0c6afedecb4ea7233d6187235df85
frederic-eynaud <frederic.eynaud@gmail.com>
2024-05-15 10:45:34 -0700

    Implement useState basics

--- 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,23 @@
+// import { useState } from 'react';
+import React from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // return <h2>useState basics</h2>;
+  // const [count, setCount] = useState(0);
+  const [count, setCount] = React.useState(0);
+  console.log(count);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <h4>Incrementor</h4>
+      <h4>New count: {count}</h4>
+      <button className="btn" type="button" onClick={handleClick}>
+        Increment
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
