4893e4a2600073436dbb8cecab2284fd4bb79cc3
frederic-eynaud <frederic.eynaud@gmail.com>
2024-05-14 17:10:59 -0700

    Implement increment counter

--- 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,18 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+  // return <h2>useState error example</h2>;
+  const handleClick = () => {
+    count++;
+    console.log(count);
+  };
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={handleClick} className="btn" type="button">
+        Increment
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
