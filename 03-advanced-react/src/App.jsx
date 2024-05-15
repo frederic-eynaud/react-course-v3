4b8449bca7ac333764d6889e03ca33cb144b9068
frederic-eynaud <frederic.eynaud@gmail.com>
2024-05-15 14:51:16 -0700

    Implement useState object

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,11 @@
 // import Starter from './tutorial/01-useState/starter/01-error-example';
-// import Starter from './tutorial/01-useState/starter/02-useState-basics';
-import Starter from './tutorial/01-useState/starter/03-useState-array';
 // import Final from './tutorial/01-useState/final/01-error-example';
+// import Starter from './tutorial/01-useState/starter/02-useState-basics';
 // import Final from './tutorial/01-useState/final/02-useState-basics';
-import Final from './tutorial/01-useState/final/03-useState-array';
+// import Starter from './tutorial/01-useState/starter/03-useState-array';
+// import Final from './tutorial/01-useState/final/03-useState-array';
+import Starter from './tutorial/01-useState/starter/04-useState-object';
+import Final from './tutorial/01-useState/final/04-useState-object';
 
 function App() {
   return (
--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,27 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState('Fred');
+  const [age, setAge] = useState('50');
+  const [hobby, setHobby] = useState('Bird watching');
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h4>Enjoys to: {hobby}</h4>
+      <button
+        className="btn"
+        onClick={() => {
+          setName('John');
+          setAge('28');
+          setHobby('Scream at the computer');
+        }}
+      >
+        Show John
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
