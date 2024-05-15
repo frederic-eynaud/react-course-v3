55bed3918bd5d15ccfac290764a0138dc809d35f
frederic-eynaud <frederic.eynaud@gmail.com>
2024-05-15 16:11:41 -0700

    Implement useState ojbect using batching

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  const [name, setName] = useState('Fred');
-  const [age, setAge] = useState('50');
-  const [hobby, setHobby] = useState('Bird watching');
+  // const [name, setName] = useState('Fred');
+  // const [age, setAge] = useState('50');
+  // const [hobby, setHobby] = useState('Bird watching');
+  const [person, setPerson] = useState({
+    name: 'Frederic',
+    age: 50,
+    hobby: 'Bird watching',
+  });
 
   return (
     <>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h4>Enjoys to: {hobby}</h4>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoys to: {person.hobby}</h4>
       <button
         className="btn"
         onClick={() => {
-          setName('John');
-          setAge('28');
-          setHobby('Scream at the computer');
+          // setName('John');
+          // setAge('28');
+          // setHobby('Scream at the computer');
+          setPerson({
+            name: 'John',
+            age: 28,
+            hobby: 'Scream at the computer',
+          });
         }}
       >
         Show John
