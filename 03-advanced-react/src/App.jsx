a6134acaf274fc21870aab6de47256891bf350aa
frederic-eynaud <frederic.eynaud@gmail.com>
2024-05-16 14:11:37 -0700

    Implement code example

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,16 @@
+import Starter from './tutorial/02-useEffect/starter/01-code-example';
+import Final from './tutorial/02-useEffect/final/01-code-example';
+<tu></tu>;
+
 function App() {
   return (
     <div className="container">
-      <h2>Advanced React</h2>
+      <h2>02 useEffect</h2>
+      <h3>code example</h3>
+      <hr />
+      <Starter />
+      <hr />
+      <Final />
     </div>
   );
 }
