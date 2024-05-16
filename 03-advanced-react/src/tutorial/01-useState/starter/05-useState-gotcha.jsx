20741f093b718cb8ca19cdc396dbafea2b46475a
frederic-eynaud <frederic.eynaud@gmail.com>
2024-05-16 12:11:23 -0700

    Implement useState gotcha

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+  return (
+    <div>
+      <h2>{value}</h2>
+      <button
+        className="btn"
+        onClick={() => {
+          setValue((value) => {
+            const newValue = value + 1;
+            return newValue;
+          });
+          console.log(value);
+        }}
+      >
+        Click me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
