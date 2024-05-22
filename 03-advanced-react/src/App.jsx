d4e1af1cac2d25de2168c44f328b7f3e7e95791e
frederic-eynaud <frederic.eynaud@gmail.com>
2024-05-21 18:15:24 -0700

    Implement multiple returns basics

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,15 @@
+import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics';
+import Final from './tutorial/03-conditional-rendering/final/01-multiple-returns-basics';
+
 function App() {
   return (
     <div className="container">
       <h2>Advanced React</h2>
+      <h3>Conditional Rendering</h3>
+      <hr />
+      <Starter />
+      <hr />
+      <Final />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,19 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
-  return <h2>Multiple Returns Basics</h2>;
+  // return <h2>Multiple Returns Basics</h2>;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  if (loading) {
+    return <h2>... waiting</h2>;
+  }
+
+  return <h2>My App</h2>;
 };
 export default MultipleReturnsBasics;
