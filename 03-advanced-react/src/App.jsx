4299a358620b50fb1cdf0328eca438400275ec60
frederic-eynaud <frederic.eynaud@gmail.com>
2024-05-22 18:24:46 -0700

    Implement user challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,14 +1,14 @@
-import ToggleChallenge from './tutorial/03-conditional-rendering/starter/06-toggle-challenge';
-import Final from './tutorial/03-conditional-rendering/final/06-toggle-challenge';
+import UserChallenge from './tutorial/03-conditional-rendering/starter/07-user-challenge';
+import Final from './tutorial/03-conditional-rendering/final/07-user-challenge';
 
 function App() {
   return (
     <div className="container">
       <h2>Advanced React</h2>
       <h3>Conditional Rendering</h3>
-      <h4>toggle challenge</h4>
+      <h4>user challenge</h4>
       <hr />
-      <ToggleChallenge />
+      <UserChallenge />
       <hr />
       <Final />
     </div>
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [visible, setVisible] = useState(false);
+  const [message, setMessage] = useState('');
+  const [buttonText, setButtonText] = useState('');
+
+  useEffect(() => {
+    setMessage(visible ? 'Person logged in' : 'Please Login');
+    setButtonText(visible ? 'Logout' : 'Login');
+  }, [visible]);
+
+  return (
+    <>
+      <h4>{message}</h4>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {buttonText}
+      </button>
+    </>
+  );
 };
 
 export default UserChallenge;
