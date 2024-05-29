d0f615fd23a8c56036890c02e5597370bd2418b2
frederic-eynaud <frederic.eynaud@gmail.com>
2024-05-29 14:37:21 -0700

    Implement user challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,14 @@
+import Starter from './tutorial/06-forms/starter/02-user-challenge';
+import Final from './tutorial/06-forms/final/02-user-challenge';
+
 function App() {
   return (
     <div className="container">
       <h2>Advanced React</h2>
+      <hr />
+      <Starter />
+      <hr />
+      <Final />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,86 @@
+import { useState } from 'react';
+import { data, people } from '../../../data';
+
 const UserChallenge = () => {
+  const [name, setName] = useState('');
+  const [users, setUsers] = useState(data);
+  const [id, setId] = useState(users.length + 1);
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+    // console.log(e.target.name, name);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setId(id + 1);
+    // const name = e.target.name.va;
+    // setUsers([...users, { id: id, name: e.target.name.value }]);
+    // setUsers([...users, { id: id, name: name }]);
+    setUsers([...users, { id, name }]);
+    // console.log(users);
+    // console.log('Submitting: ', name);
+    // console.log('Submitting: ', id, e.target.name.value);
+    // e.target.name.value = '';
+    setName('');
+  };
+
+  const removeUser = (idToRemove) => {
+    // console.log('Remove: ', idToRemove, users);
+    // users
+    //   .filter((user) => {
+    //     return user.id != idToRemove;
+    //   })
+    //   .map((user) => console.log('id: ', user.id, ', name: ', user.name));
+    setUsers(
+      users.filter((user) => {
+        return user.id != idToRemove;
+      })
+    );
+    // console.log(users);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            value={name}
+            id="name"
+            onChange={handleChange}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       {/* render users below */}
+      <ul>
+        {users.map((user) => {
+          // console.log(user.id, user.name);
+          return (
+            <li key={user.id}>
+              <h4>{user.name}</h4>
+              <button
+                className="btn"
+                onClick={() => {
+                  // console.log('Remove: ', user.name, user);
+                  removeUser(user.id);
+                }}
+              >
+                remove
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
