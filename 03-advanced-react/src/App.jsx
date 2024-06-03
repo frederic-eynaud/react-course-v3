58b2530c85795267c72daef6bb1fbc6a491e0159
frederic-eynaud <frederic.eynaud@gmail.com>
2024-06-03 16:04:45 -0700

    Implement 02-fetch-data

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,14 @@
+import Starter from './tutorial/08-custom-hooks/starter/02-fetch-data';
+import Final from './tutorial/08-custom-hooks/final/02-fetch-data';
+
 function App() {
   return (
     <div className="container">
       <h2>Advanced React</h2>
+      <hr />
+      <Starter />
+      <hr />
+      <Final />
     </div>
   );
 }
---
+++ 03-advanced-react/src/tutorial/08-custom-hooks/starter/useFetchData.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+const useFetchData = (url) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(url);
+        // console.log(resp);
+        if (!resp.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+
+        const user = await resp.json();
+        setData(user);
+      } catch (error) {
+        setIsError(true);
+        // console.log(error);
+      }
+      // hide loading
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return { isLoading, isError, user: data };
+};
+
+export default useFetchData;
